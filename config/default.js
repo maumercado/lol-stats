6defc04d5626113d1e6b510b082117017f0191ff
Mauricio Mercado <mau@maumercado.com>
2017-11-30 23:17:59 -0800

    add env port to server

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
 module.exports = {
-    server: { riot_api_key: process.env.RIOT_API_KEY, port: 3001 },
+    server: { riot_api_key: process.env.RIOT_API_KEY, port: process.env.PORT },
     client: {}
 };
